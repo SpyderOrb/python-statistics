a16f4c4adfc1f9410ac03d757fb3bb9d367509c7
Oleksandr Plachynda <alex.plachinda@gmail.com>
2021-04-03 21:17:36 +0200

    arithmetic average

--- SzerRozdPunkPANDAS.py
+++ SzerRozdPunkPANDAS.py
@@ -11,6 +11,7 @@ Fi = list(
     map(int, input('Wpisz liczebnosci (fi) z jakimi te wartosci wystepuja: ').strip().split())
 )[:n]
 
+# Xi, Fi ---------------------------------------------------
 start_dict = {'Xi': Xi, 'Fi': Fi}
 
 # FiXi -----------------------------------------------------
@@ -19,6 +20,7 @@ start_dict.update({'FiXi': FiXi})
 
 # Xi2 ------------------------------------------------------
 Xi2 = [i**2 for i in Xi]
+start_dict.update({'Xi2': Xi2})
 
 # FiXi2 ----------------------------------------------------
 FiXi2 = [i * j for i, j in zip(Xi2, Fi)]
@@ -26,17 +28,32 @@ start_dict.update({'FiXi2': FiXi2})
 
 # FiKu -----------------------------------------------------
 FiKu = list(accumulate(Fi))
-start_dict.update({'FiXi2': FiXi2})
+start_dict.update({'FiKu': FiKu})
 
-# FiKu -----------------------------------------------------
+# ???
+# Detailed series ------------------------------------------
+Detailed_series = []
+for i, j in zip(Xi, Fi):
+    count = 0
+    while (j != count):
+        Detailed_series.append(i)
+        count += 1
+print(Detailed_series)
+# ???
 
 
-# pd_start_dict = pd.DataFrame(data=start_dict)
-# print(pd_start_dict)
+pd_start_dict = pd.DataFrame(data=start_dict, dtype=np.int64)
+# print(pd_start_dict, '\n')
 
+# Descriptive measures *****************************************************************************
 
-# # Добавим все суммы в конце списка
-# row_sum = pd.Series({'Fi': sum(Fi), 'FiXi2': sum(FiXi2)}, name='sum')
-# pd_start_dict = pd_start_dict.append(row_sum)
+# Mean ------------------------------------------
+arithmeticAverage = pd_start_dict.loc[:, 'FiXi'].sum() / sum(Fi)
+print('Srednia arytmetyczna: ', arithmeticAverage, '\n')
 
-# print(pd_start_dict)
+# Добавим все суммы в конце списка
+row_sum = pd.Series({'Fi': sum(Fi), 'FiXi': sum(FiXi),
+                    'FiXi2': sum(FiXi2)}, name='sum')
+pd_start_dict = pd_start_dict.append(row_sum)
+# Сделать все числа из float в int
+print(pd_start_dict)
