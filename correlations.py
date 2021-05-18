04d6f1232058ee955cbbed019150a553f5dfcb8b
Oleksandr Plachynda <alex.plachinda@gmail.com>
2021-05-18 23:38:29 +0200

    finished study of the community due to two features

--- correlations.py
+++ correlations.py
@@ -1,3 +1,4 @@
+from interval_series import toDecimal
 import math
 import os
 import numpy as np
@@ -29,25 +30,26 @@ def run_correlations():
     # Współczynnik korelacji (korelogram)
     # Liniowa funkcja regresji
 
-    # n = int(input('Wpisz rozmiar tablicy: '))
+    n = int(input('  Wpisz rozmiar tablicy: '))
     # Xi = list(map(int, input(
-    #     'Wpisz wartosc cechy Xi oddzielone spacja: ').strip().split()))[:n]
+    #     '  Wpisz wartosc cechy Xi oddzielone spacja: ').strip().split()))[:n]
     # Yi = list(map(int, input(
-    #     'Wpisz wartosc cechy Yi oddzielone spacja: ').strip().split()))[:n]
-    Xi = [16, 25, 24, 50, 60]
-    Yi = [95, 163, 250, 297, 335]
+    #     '  Wpisz wartosc cechy Yi oddzielone spacja: ').strip().split()))[:n]
     decimal_places = int(
         input('  Ile chcesz miec miejsc po pzecinku (wartosc domyslna to 4): ') or "4")
 
+    Xi = [16, 25, 24, 50, 60]
+    Yi = [95, 163, 250, 297, 335]
+
+    # Сreating a dataframe
     df_correlation = pd.DataFrame(list(zip(Xi, Yi)), columns=['Xi', 'Yi'])
+
     # Xi, Yi averages -----------------------------------------------------
-    # ---------------------------------------------------------------------
-    # ---------------------------------------------------------------------
     # ---------------------------------------------------------------------
     Xi_avg = df_correlation.loc[:, 'Xi'].mean()
     Yi_avg = df_correlation.loc[:, 'Yi'].mean()
 
-    # Xi - Xi_avg, Yi - Yi_avg --------------------------------------------
+    # Xi - Xi_avg, Yi - Yi_avg, -------------------------------------------
     Xi_diff_Xi_avg = [(i - Xi_avg) for i in Xi]
     Yi_diff_Yi_avg = [(i - Yi_avg) for i in Yi]
     df_correlation['Xi-Xi_avg'] = Xi_diff_Xi_avg
@@ -64,21 +66,14 @@ def run_correlations():
     df_correlation['(Xi-Xi_avg)^2'] = Xi_diff_Xi_avg2
     df_correlation['(Yi-Yi_avg)^2'] = Yi_diff_Yi_avg2
 
-    df_correlation_row_sum = pd.DataFrame(
-        {'(Xi-Xi_avg)(Yi-Yi_avg)': sum(Xi_diff_Xi_avg_MULT_Yi_diff_Yi_avg),
-         '(Xi-Xi_avg)^2': sum(Xi_diff_Xi_avg2),
-         '(Yi-Yi_avg)^2': sum(Yi_diff_Yi_avg2)
-         },
-        index=['sum']
-    )
-
+    # Descriptive measures ********************************************************************************
     # Pearson linear correlation coefficient-------------------------------
-    r = round((sum(Xi_diff_Xi_avg_MULT_Yi_diff_Yi_avg) /
-               (math.sqrt(sum(Xi_diff_Xi_avg2) * sum(Yi_diff_Yi_avg2)))), 2)
+    r = (sum(Xi_diff_Xi_avg_MULT_Yi_diff_Yi_avg) /
+         (math.sqrt(sum(Xi_diff_Xi_avg2) * sum(Yi_diff_Yi_avg2))))
 
     # Wyraz wolny (a) i wsp regresji (b)-----------------------------------
-    b = round((sum(Xi_diff_Xi_avg_MULT_Yi_diff_Yi_avg) / sum(Xi_diff_Xi_avg2)), 2)
-    a = round(Yi_avg - b * Xi_avg, 2)
+    b = (sum(Xi_diff_Xi_avg_MULT_Yi_diff_Yi_avg) / sum(Xi_diff_Xi_avg2))
+    a = Yi_avg - b * Xi_avg
 
     # Zmiana wartosci cechy ΔX---------------------------------------------
     print('  Chcesz podac zmiany wartosci cechy ΔX (wpisz 0 jezeli nie chcech):', end=' ')
@@ -90,18 +85,60 @@ def run_correlations():
         deltaYr = 'skipped...'
 
     # Prognozowanie -------------------------------------------------------
-    print(' Wpisz wartosc predykatora X*:', end=' ')
+    print('  Wpisz wartosc predykatora X*:', end=' ')
     Xpredictor = int(input() or "0")
-    if (deltaX != 0):
+    if (Xpredictor != 0):
         Ypredictor = a + b * Xpredictor
         YpredictorR = toRoundDecimal(Ypredictor, decimal_places)
     else:
         YpredictorR = 'skipped...'
 
+    # ŷ, Yi - ŷ, (Yi - ŷ)^2 -----------------------------------------------
+    Yteoretyczne = [(a + b * i) for i in Xi]
+    df_correlation['ŷ'] = Yteoretyczne
+    Yi_diff_Yteoretyczne = [(i - j) for i, j in zip(Yi, Yteoretyczne)]
+    Yi_diff_Yteoretyczne2 = [i ** 2 for i in Yi_diff_Yteoretyczne]
+    df_correlation['(Yi - ŷ)^2'] = Yi_diff_Yteoretyczne2
+    # Wariancja resztowa --------------------------------------------------
+    Se2 = (1/(n-2) * sum(Yi_diff_Yteoretyczne2))
+
+    # Odchylenie standardowe reszt ----------------------------------------
+    std = math.sqrt(Se2)
+
+    # Współczynnik zmienności resztowej -----------------------------------
+    cv = std / abs(Yi_avg) * 100
+    if (cv < 10):
+        cv_message = '  Vse < 10%.  Ten model jest dobrze dopasowany do danych.'
+    else:
+        cv_message = '  Vse > 10%.  Ten model jest slabo dopasowany do danych.'
+
+    # Współczynnik zbieżności (indeterminacji) ----------------------------
+    oIo2 = sum(Yi_diff_Yteoretyczne2) / sum(Yi_diff_Yi_avg2)
+    oIo2_proc = oIo2 * 100
+
+    # Współczynnik determinacji -------------------------------------------
+    R2 = 1 - oIo2
+    R2_proc = R2 * 100
+
+    # Dataframes
+    df_correlation_row_sum = pd.DataFrame(
+        {'(Xi-Xi_avg)(Yi-Yi_avg)': sum(Xi_diff_Xi_avg_MULT_Yi_diff_Yi_avg),
+         '(Xi-Xi_avg)^2': sum(Xi_diff_Xi_avg2),
+         '(Yi-Yi_avg)^2': sum(Yi_diff_Yi_avg2),
+         '(Yi - ŷ)^2': sum(Yi_diff_Yteoretyczne2)
+         },
+        index=['sum']
+    )
+
     df_correlation_descriptive_measures = pd.DataFrame(
         {'wsp korelacji (r)': toRoundDecimal(r, decimal_places),
          'wsp regresji (b)': toRoundDecimal(b, decimal_places),
-         'wyraz wolny (a)': toRoundDecimal(a, decimal_places)
+         'wyraz wolny (a)': toRoundDecimal(a, decimal_places),
+         'Se^2': toRoundDecimal(Se2, decimal_places),
+         'Se': toRoundDecimal(std, decimal_places),
+         'Vse (%)': toRoundDecimal(cv, decimal_places),
+         'φ^2 (%)': toRoundDecimal(oIo2_proc, decimal_places),
+         'R^2 (%)': toRoundDecimal(R2_proc, decimal_places)
          },
         index=['miary statystyki']
     ).astype(np.float64)
@@ -109,11 +146,12 @@ def run_correlations():
     display(df_correlation)
     print(f'  Xi srednia:  {Xi_avg} | Yi srednia: {Yi_avg}')
     display(df_correlation_row_sum)
-    print(f'  Rownanie regresji: Y = {a} {sign(b)} {b} * X')
+    print(
+        f'  Rownanie regresji: Y = {toRoundDecimal(a, decimal_places)} {sign(b)} {toRoundDecimal(b, decimal_places)} * X')
     display(df_correlation_descriptive_measures)
+    print(cv_message)
     print('  ΔY: ', deltaYr)
     print('  *X: ', YpredictorR)
-    os.system('pause')
 
 
 if __name__ == "__main__":
