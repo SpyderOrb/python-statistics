67cd530ff493d4d6116a2e7b935483efe242f6bf
Oleksandr Plachynda <alex.plachinda@gmail.com>
2021-05-18 23:35:59 +0200

    Added new functionality (interdependence of two characteristics

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import point_series
 import interval_series
+import correlations
 logo = '''
 
     _____ __        __             __        __                        _
    / ___// /_____ _/ /___  _______/ /___  __/ /______ _   ____  ____  (_)________ _      ______ _
    \__ \/ __/ __ `/ __/ / / / ___/ __/ / / / //_/ __ `/  / __ \/ __ \/ / ___/ __ \ | /| / / __ `/
   ___/ / /_/ /_/ / /_/ /_/ (__  ) /_/ /_/ / ,< / /_/ /  / /_/ / /_/ / (__  ) /_/ / |/ |/ / /_/ /
- /____/\__/\__,_/\__/\__, /____/\__/\__, /_/|_|\__,_/   \____/ .___/_/____/\____/|__/|__/\__,_/  version 1.11
+ /____/\__/\__,_/\__/\__, /____/\__/\__, /_/|_|\__,_/   \____/ .___/_/____/\____/|__/|__/\__,_/  version 1.2
                     /____/         /____/                   /_/                                     
 '''
 
@@ -17,8 +18,8 @@ def cls():
 
 
 def go_back(argument='y'):
-    print('\nWpisz \'y\' potem nacisnij \'Enter\' jesli chcesz wrocic do menu glownego')
-    argument = input(':')
+    print('\nWpisz \'y\' potem nacisnij \'Enter\' jesli chcesz wrocic do menu glownego:', end=' ')
+    argument = input()
     funcmap = {'y': main()}
     if (argument == 'y'):
         funcmap['y']
@@ -33,10 +34,11 @@ def main():
 
       1 - szereg wazony (rozdzielczy punktowy)
       2 - szereg rozdzielczy przedziałowy
+      3 - wspolzaleznosc liniowa dwoch cech
 
     Nacisnij dowolny klawisz aby zamknac.
     ''')
-    argument = int(input('    Podaj argument [1 lub 2]: '))
+    argument = int(input('    Podaj argument [1, 2 lub 3]: '))
     if (argument == 1):
         cls()
         try:
@@ -49,6 +51,14 @@ def main():
         cls()
         try:
             interval_series.run_interval_series()
+        except Exception as ex:
+            print('\n\t', ex, '---> (Innymi slowy, wystapil blad, zacznij od nowa...)')
+            go_back()
+        go_back(argument)
+    elif (argument == 3):
+        cls()
+        try:
+            correlations.run_correlations()
         except Exception as ex:
             print('\n\t', ex, '---> (Innymi slowy, wystapil blad, zacznij od nowa...)')
             go_back()
